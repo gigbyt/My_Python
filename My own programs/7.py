cfda841e8b371d9f11cb3ccc724413d460952e87
Daksh Chand <shadowunknown009@gmail.com>
2025-04-07 20:46:02 +0530

    understanding arays

---
+++ My own programs/7.py
@@ -0,0 +1,9 @@
+import numpy as np
+zeros = np.zeros((2,3))
+print("Zeros Array:\n", zeros)
+ones = np.ones((3,2))
+print("Ones Array:\n", ones)
+range_array = np.arange(10,20)
+print("Range Array:\n", range_array)
+random_array = np.random.rand(3, 3)
+print("Random Array:\n", random_array)
