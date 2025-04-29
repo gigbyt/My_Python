0662a682818b806f0cc08e53712271268847aeab
Daksh Chand <shadowunknown009@gmail.com>
2025-04-29 21:05:15 +0530

    Udating Fibonacci sieres func

--- My_Package/Arithmatical.py
+++ My_Package/Arithmatical.py
@@ -37,8 +37,10 @@ def divide_numbers(n1, n2):
     except Exception as e:
         print("invalid input")
 
-def fibonacci_numbers (n):
-    fibonacci_seiries = [0, 1]
-    while len(fibonacci_seiries) < n:
-        fibonacci_seiries.append(fibonacci_seiries[-1] + fibonacci_seiries[-2])
-    return fibonacci_seiries
+def fibonacci_numbers(n):
+  fib_series = []
+  a, b = 0, 1
+  for _ in range(n):
+    fib_series.append(a)
+    a, b = b, a + b
+  return fib_series
