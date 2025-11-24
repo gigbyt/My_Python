620440d81e98c894f3f42ce661709b4ff9ec20ca
Daksh Chand <shadowunknown009@gmail.com>
2025-11-24 22:42:35 +0530

    fixing bugs

--- Python Project Programs (School)/For loop Pattern based programs/program8.py
+++ Python Project Programs (School)/For loop Pattern based programs/program8.py
@@ -1,5 +1,5 @@
 n = int(input("Enter the number of times the pattern should be repeated: "))
 for i in range(1, n+1):
-    for a in range (n, i, -1):
+    for a in range (n+1, i, -1):
         print(i, end=" ")
     print()
