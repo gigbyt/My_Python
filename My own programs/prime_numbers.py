66e9e544327c8e5b8d65a4ff5303aeee37d9a98d
Daksh Chand <shadowunknown009@gmail.com>
2026-02-22 19:16:27 +0530

    simplifying

--- My own programs/prime_numbers.py
+++ My own programs/prime_numbers.py
@@ -1,17 +1,7 @@
-def checkPrime(n):
-  if n == 1:
-    return False
-  if n == 2:
-    return True
-  for i in range(2, n):
-    if n % i == 0:
-      return False
-  if n % i != 0:
-    return True
-
 n = int(input("Enter any number: "))
-r = checkPrime(n)
-if r == True:
-  print("The number is prime")
+for i in range(2, n):
+  if n%i == 0:
+    print("not prime")
+    break
 else:
-  print("The number is not prime")
+  print("prime")
